665eab7a14b9697b8ebd861fdc92c44213895cc2
John Jacobsen <john@mail.npxdesigns.com>
2012-02-19 11:58:27 -0600

    Implement setuptools/distutil install script and fix Python 2.6 compatilibity issue

--- clojure.py
+++ clojure.py
@@ -14,6 +14,7 @@ import py.clojure.lang.rt as RT
 from py.clojure.lang.compiler import Compiler
 from py.clojure.lang.symbol import Symbol
 
+VERSION = "0.0.0"
 
 def requireClj(filename, stopafter = None):
     with open(filename) as fl:
