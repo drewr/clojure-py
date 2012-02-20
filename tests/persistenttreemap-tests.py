9c83d4799c5ce5776ccce509dfd32015d35abe16
Eric Shull <eric.shull@gmail.com>
2012-02-20 15:13:59 -0500

    Test cleanup.

--- tests/persistenttreemap-tests.py
+++ tests/persistenttreemap-tests.py
@@ -5,29 +5,22 @@ from py.clojure.lang.persistenttreemap import PersistentTreeMap
 
 class PresistentTreeMapTests(unittest.TestCase):
     def testAssoc(self):
-        m = PersistentTreeMap()
-        m2 = m.assoc('a', 1)
-        self.assertTrue(m2.containsKey('a'))
+        m = PersistentTreeMap().assoc('a', 1)
+        self.assertTrue(m.containsKey('a'))
 
     def testWithout(self):
-        m = PersistentTreeMap()
-        m2 = m.assoc('a', 1)
-        m3 = m2.without('a')
-        self.assertFalse(m3.containsKey('a'))
+        m = PersistentTreeMap().assoc('a', 1).without('a')
+        self.assertFalse(m.containsKey('a'))
 
     def testSeq(self):
         self.assertEqual(PersistentTreeMap().seq(), None)
 
-        m = PersistentTreeMap()
-        m1 = m.assoc('a', 1)
-        m2 = m1.assoc('b', 2)
-        self.assertEqual(m2.seq().first().key(), 'a')
-        self.assertEqual(m2.seq().next().first().key(), 'b')
+        m = PersistentTreeMap().assoc('a', 1).assoc('b', 2)
+        self.assertEqual(m.seq().first().key(), 'a')
+        self.assertEqual(m.seq().next().first().key(), 'b')
 
     def testKeys(self):
-        m = PersistentTreeMap()
-        m1 = m.assoc('a', 1)
-        m2 = m1.assoc('b', 2)
-        keys = m2.keys()
+        m = PersistentTreeMap().assoc('a', 1).assoc('b', 2)
+        keys = m.keys()
         self.assertEqual(keys.next(), 'a')
         self.assertEqual(keys.next(), 'b')
